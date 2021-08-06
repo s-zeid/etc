9d8f12f21833cbe634ba9e454464b0f5bec977d3
S. Zeid <s@zeid.me>
2021-08-06 08:40:45 -0500

    [zsh] make RPS1 date/time colors/formats configurable; make all PS1/RPS1 prompt colors overridable before `03-prompt.zsh` runs; properly set colors when their types (24-bit/256/basic) are mixed

--- zsh/03-prompt.zsh
+++ zsh/03-prompt.zsh
@@ -1,12 +1,24 @@
-PS1_COLOR_24bit='\x1b[38;2;171;127;204m'
-PS1_COLOR_256='\x1b[38;5;140m'
-PS1_COLOR_BASIC='DARK_MAGENTA'
+# color and format options
+
+PS1_COLOR_24bit="${PS1_COLOR_24bit-\\x1b[38;2;171;127;204m}"
+PS1_COLOR_256="${PS1_COLOR_256-\\x1b[38;5;140m}"
+PS1_COLOR_BASIC="${PS1_COLOR_BASIC-DARK_MAGENTA}"
 
 PS1_USER="${PS1_USER-%n}"
 PS1_USER="$PS1_USER${PS1_USER:+@}"
 PS1_MACHINE="${PS1_MACHINE-%m}"
 PS1_SIGIL="${PS1_SIGIL-\$(pwd=\$(pwd); ([ x\"\$pwd\" = x\"/\" ]) && true || echo /)}"
 #PS1_SIGIL='%(!.#.$)'  # like `\$` in bash
+
+RPS1_DATE_COLOR_24bit="${RPS1_DATE_COLOR_24bit-\\x1b[38;5;248m}"
+RPS1_DATE_COLOR_256="${RPS1_DATE_COLOR_256-\\x1b[38;5;248m}"
+RPS1_DATE_COLOR_BASIC="${RPS1_DATE_COLOR_BASIC-DARK_WHITE}"
+RPS1_DATE_FORMAT="${RPS1_DATE_FORMAT-%Y-%m-%d}"
+
+RPS1_TIME_COLOR_24bit="${RPS1_TIME_COLOR_24bit-\\x1b[38;5;251m}"
+RPS1_TIME_COLOR_256="${RPS1_TIME_COLOR_256-\\x1b[38;5;251m}"
+RPS1_TIME_COLOR_BASIC="${RPS1_TIME_COLOR_BASIC-WHITE}"
+RPS1_TIME_FORMAT="${RPS1_TIME_FORMAT-%H:%M:%S}"
 
 
 # setup basic color variables
@@ -39,29 +51,49 @@ if ([ x"$COLORTERM" = x"truecolor" ] || [ x"$COLORTERM" = x"24bit" ]) \
  else
   PS1_COLOR="$(eval echo "\$_COLOR_$PS1_COLOR_BASIC")"
  fi
- RPS1_COLOR_DATE="%{$(printf '\x1b[38;5;248m')%}"
- RPS1_COLOR_TIME="%{$(printf '\x1b[38;5;251m')%}"
-elif (printf '%s\n' "$TERM" | grep -q -e '256') && [ x"$PS1_COLOR_256" != x"" ]; then
+ if [ x"$RPS1_DATE_COLOR_24bit" != x"" ]; then
+  RPS1_DATE_COLOR="%{$(printf "$RPS1_DATE_COLOR_24bit")%}"
+ elif [ x"$RPS1_DATE_COLOR_256" != x"" ]; then
+  RPS1_DATE_COLOR="%{$(printf "$RPS1_DATE_COLOR_256")%}"
+ else
+  RPS1_DATE_COLOR="$(eval echo "\$_COLOR_$RPS1_DATE_COLOR_BASIC")"
+ fi
+ if [ x"$RPS1_TIME_COLOR_24bit" != x"" ]; then
+  RPS1_TIME_COLOR="%{$(printf "$RPS1_TIME_COLOR_24bit")%}"
+ elif [ x"$RPS1_TIME_COLOR_256" != x"" ]; then
+  RPS1_TIME_COLOR="%{$(printf "$RPS1_TIME_COLOR_256")%}"
+ else
+  RPS1_TIME_COLOR="$(eval echo "\$_COLOR_$RPS1_TIME_COLOR_BASIC")"
+ fi
+elif (printf '%s\n' "$TERM" | grep -q -e '256'); then
  # 256 colours
  if [ x"$PS1_COLOR_256" != x"" ]; then
   PS1_COLOR="%{$(printf "$PS1_COLOR_256")%}"
  else
   PS1_COLOR="%{$(printf "$(eval echo "\$_COLOR_$PS1_COLOR_BASIC")")%}"
  fi
- RPS1_COLOR_DATE="%{$(printf '\x1b[38;5;248m')%}"
- RPS1_COLOR_TIME="%{$(printf '\x1b[38;5;251m')%}"
+ if [ x"$RPS1_DATE_COLOR_256" != x"" ]; then
+  RPS1_DATE_COLOR="%{$(printf "$RPS1_DATE_COLOR_256")%}"
+ else
+  RPS1_DATE_COLOR="%{$(printf "$(eval echo "\$_COLOR_$RPS1_DATE_COLOR_BASIC")")%}"
+ fi
+ if [ x"$RPS1_TIME_COLOR_256" != x"" ]; then
+  RPS1_TIME_COLOR="%{$(printf "$RPS1_TIME_COLOR_256")%}"
+ else
+  RPS1_TIME_COLOR="%{$(printf "$(eval echo "\$_COLOR_$RPS1_TIME_COLOR_BASIC")")%}"
+ fi
 else
  # basic colours
  PS1_COLOR="%{$(printf "$(eval echo "\$_COLOR_$PS1_COLOR_BASIC")")%}"
- RPS1_COLOR_DATE="%{$(printf "$_COLOR_DARK_WHITE")%}"
- RPS1_COLOR_TIME="%{$(printf "$_COLOR_WHITE")%}"
+ RPS1_DATE_COLOR="%{$(printf "$(eval echo "\$_COLOR_$RPS1_DATE_COLOR_BASIC")")%}"
+ RPS1_TIME_COLOR="%{$(printf "$(eval echo "\$_COLOR_$RPS1_TIME_COLOR_BASIC")")%}"
 fi
 
 
 # set PS1 and RPS1
 
 PS1="$PS1_COLOR$PS1_USER$PS1_MACHINE:%~$PS1_SIGIL %{$(printf "$_COLOR_NONE")%}"
-RPS1="%{ %}$RPS1_COLOR_DATE%D{%Y-%m-%d} $RPS1_COLOR_TIME%D{%H:%M:%S}%{$(printf "$_COLOR_NONE"'\x08')%}"
+RPS1="%{ %}$RPS1_DATE_COLOR%D{$RPS1_DATE_FORMAT} $RPS1_TIME_COLOR%D{$RPS1_TIME_FORMAT}%{$(printf "$_COLOR_NONE"'\x08')%}"
 setopt PROMPT_SUBST
 
 
@@ -69,8 +101,10 @@ setopt PROMPT_SUBST
 
 unset \
  PS1_COLOR_24bit PS1_COLOR_256 PS1_COLOR_BASIC \
- PS1_USER PS1_MACHINE PS1_SIGIL \
- PS1_COLOR RPS1_COLOR_DATE RPS1_COLOR_TIME \
+ RPS1_DATE_COLOR_24bit RPS1_DATE_COLOR_256 RPS1_DATE_COLOR_BASIC \
+ RPS1_TIME_COLOR_24bit RPS1_TIME_COLOR_256 RPS1_TIME_COLOR_BASIC \
+ PS1_COLOR PS1_USER PS1_MACHINE PS1_SIGIL \
+ RPS1_DATE_COLOR RPS1_DATE_FORMAT RPS1_TIME_COLOR RPS1_TIME_FORMAT \
  _COLOR_NONE
 
 for color in RED YELLOW GREEN CYAN BLUE MAGENTA WHITE DEFAULT; do
