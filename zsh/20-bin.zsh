ce9ef6fc95d6993478364b81a112c27825b6250c
S. Zeid <s@zeid.me>
2024-03-08 10:12:45 -0600

    [zsh] Load _completions.zsh from ~/bin and ~/bin/private

---
+++ zsh/20-bin.zsh
@@ -0,0 +1,6 @@
+if [ -f "$HOME/bin/_completions.zsh" ]; then
+  . "$HOME/bin/_completions.zsh"
+fi
+if [ -f "$HOME/bin/private/_completions.zsh" ]; then
+  . "$HOME/bin/private/_completions.zsh"
+fi
