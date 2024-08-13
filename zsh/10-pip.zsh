70dea138e3cfafc83f42ebfc8cc40d27904962ee
S. Zeid <s@zeid.me>
2024-08-13 07:14:45 -0500

    [zsh] Use alias to set pip environment variables

--- zsh/10-pip.zsh
+++ zsh/10-pip.zsh
@@ -1,2 +1,6 @@
-export PIP_USER=true
-export PIP_BREAK_SYSTEM_PACKAGES=true
+pip_env_vars='PIP_USER=true PIP_BREAK_SYSTEM_PACKAGES=true'
+
+alias pip="env $pip_env_vars pip"
+alias pip3="env $pip_env_vars pip3"
+
+unset pip_env_vars
