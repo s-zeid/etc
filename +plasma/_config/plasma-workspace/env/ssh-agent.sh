8d0b5f059e9a0ec6e8fa23aefc839ebf74d4d16d
S. Zeid <s@zeid.me>
2025-06-11 11:42:38 -0500

    [+plasma:ssh_agent] add shebang lines and executable permissions

--- +plasma/_config/plasma-workspace/env/ssh-agent.sh
+++ +plasma/_config/plasma-workspace/env/ssh-agent.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -e
+
 if [ -z "$SSH_AUTH_SOCK" ]; then
   eval "$(ssh-agent -s)"
   export SSH_AUTH_FROM='etc/+plasma'
--- +plasma/_config/plasma-workspace/shutdown/ssh-agent.sh
+++ +plasma/_config/plasma-workspace/shutdown/ssh-agent.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -e
+
 if [ x"$SSH_AUTH_FROM" = x"etc/+plasma" ] && [ -n "$SSH_AGENT_PID" ]; then
   ssh-agent -k
 fi
